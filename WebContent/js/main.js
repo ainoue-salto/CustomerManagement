29052fadb4acc4ae782823c923c21337c681427f
ayaka <ayaka1818inoue@gmail.com>
2024-01-16 12:16:02 +0900

    Revert "update:バリデーションに関するコメントアウト追記"
    This reverts commit 9909bfee09b94b05159fe0834f07a6a788443dc5.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -8,11 +8,10 @@ window.addEventListener("input", function(e){
 	var form = document.getElementById("email");
 	/*結果出力用の要素*/
 	var result = document.getElementById("result");
-	/*メールアドレスの(ユーザー名@ドメイン名) 正規表現*/
-	/*先頭1文字目に半角英数字 ＋ それ以降に半角英数字または_.- ＋ @ ＋ 半角英数字または_.- ＋ . ＋ 半角英数字*/
+	/*メールアドレスのパターン 正規表現*/
 	var pattern = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
 	
-    /*メールアドレスのパターン(ユーザー名@ドメイン名)にマッチするかチェック*/
+    /*メールアドレスのパターンにマッチするかチェック*/
     if (pattern.test(form.value)) {
          /*パターンにマッチした場合*/
          result.textContent = "正しいメールアドレスです";
