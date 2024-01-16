9909bfee09b94b05159fe0834f07a6a788443dc5
ayaka <ayaka1818inoue@gmail.com>
2024-01-16 12:13:28 +0900

    update:バリデーションに関するコメントアウト追記

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -8,10 +8,11 @@ window.addEventListener("input", function(e){
 	var form = document.getElementById("email");
 	/*結果出力用の要素*/
 	var result = document.getElementById("result");
-	/*メールアドレスのパターン 正規表現*/
+	/*メールアドレスの(ユーザー名@ドメイン名) 正規表現*/
+	/*先頭1文字目に半角英数字 ＋ それ以降に半角英数字または_.- ＋ @ ＋ 半角英数字または_.- ＋ . ＋ 半角英数字*/
 	var pattern = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
 	
-    /*メールアドレスのパターンにマッチするかチェック*/
+    /*メールアドレスのパターン(ユーザー名@ドメイン名)にマッチするかチェック*/
     if (pattern.test(form.value)) {
          /*パターンにマッチした場合*/
          result.textContent = "正しいメールアドレスです";
